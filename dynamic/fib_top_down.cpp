a9bdd76ad9bf6e0c83b0a4359b8a83aaeed28a56
sanyache <sanyache75@gmail.com>
2024-06-25 17:53:21 +0300

    fix fib top

--- dynamic/fib_top_down.cpp
+++ dynamic/fib_top_down.cpp
@@ -8,9 +8,8 @@ uint64_t fib_dynamic(int i){
     static uint64_t knownF[maxN];
     cout<<"i="<<i<<'\t';
     if(knownF[i] != 0) return knownF[i];
-    int t = i;
-
-    if(i<=0) return 0;
+    if(i<=0 ) return 0;
+    if(i == 1) return 1;
     if(i>1) return knownF[i] = fib_dynamic(i-1) + fib_dynamic(i-2);
 
 }
@@ -19,7 +18,7 @@ int main(){
 
     int n;
     cin>>n;
-    for(int i=0; i<=4; i++){
+    for(int i=0; i<=n; i++){
         cout<<i<<'\t'<<fib_dynamic(i)<<'\n';
     }
     return 0;
