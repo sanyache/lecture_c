83a2f7e243305a075a78483cd7d532903dda1c5b
sanyache <sanyache75@gmail.com>
2024-10-05 20:30:41 +0300

    ring

---
+++ intro/ring.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+      for(int8_t i=100; i<120; i++){
+
+        cout<<i<<' ';
+
+    }
+//    for(int8_t i=0; i<=128; i++){
+//        cout<<int(i)<<' ';
+//    }
+//    for(uint8_t i=10; i>=0; i--){
+//        cout<<int(i)<<' ';
+//
+//    }
+    size_t len = 10;
+    int ind = -1;
+    cout<<(len > ind)<<'\n';
+    return 0;
+}
