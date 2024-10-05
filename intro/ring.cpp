07eac359424b4ec092037b9ad53ad5c33dfdfe67
sanyache <sanyache75@gmail.com>
2024-10-05 20:48:41 +0300

    init value

--- intro/ring.cpp
+++ intro/ring.cpp
@@ -16,6 +16,10 @@ int main(){
 //        cout<<int(i)<<' ';
 //
 //    }
+    int max_el;
+    int n = 10;
+    int m = max(max_el, n);
+    cout<<m;
     size_t len = 10;
     int ind = -1;
     cout<<(len > ind)<<'\n';
