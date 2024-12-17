22275487ed950fa623073d4a27363a89ae59ee8a
sanyache <sanyache75@gmail.com>
2024-12-17 21:29:00 +0200

    e2905 bad

--- segment tree/e2905.cpp
+++ segment tree/e2905.cpp
@@ -1,87 +1,88 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void resize(vector<int64_t> &arr){
-    int64_t len = pow(2, ceil(log2(arr.size())));
+void resize(vector<uint32_t> &arr){
+    uint32_t len = pow(2, ceil(log2(arr.size())));
     arr.resize(len, 0);
 }
 
 class Tree{
     public:
         
-        int64_t n;
-        vector<int64_t>seg_t;
-        Tree(vector<int64_t> &arr){
+        uint32_t n;
+        vector<uint32_t>seg_t;
+        Tree(vector<uint32_t> &arr){
             n = arr.size();
             seg_t.resize(2*n);
             build(arr, 0, 0, n);
         }
         
-        int64_t sum(int64_t l, int64_t r){
+        uint32_t sum(uint32_t l, uint32_t r){
             return _sum(l, r, 0, 0, n);
         }
-        void update(int64_t ind){
-            _update(ind, 0, 0, n);
+        void update(uint32_t l, uint32_t r){
+            _update(l, r, 0, 0, n);
         }
-        void print64_t_tree(){
+        void print_tree(){
             for(auto el: seg_t){
                 cout<<el<<' ';
             }
             cout<<'\n';
         }
     private:
-        void build(vector<int64_t> &arr, int64_t node, int64_t left, int64_t right){
+        void build(vector<uint32_t> &arr, uint32_t node, uint32_t left, uint32_t right){
             if (left == right - 1){
                 seg_t[node] = arr[left];
             } else {
-                int64_t mid = (left + right)/2;
-                build(arr, node*2 + 1, left, mid);
-                build(arr, node*2 + 2, mid, right);
-                seg_t[node] = seg_t[node*2+1] + seg_t[node*2+2];
+                uint32_t mid = (left + right)>>1;
+                build(arr, (node<<1)+ 1, left, mid);
+                build(arr, (node<<1) + 2, mid, right);
+                seg_t[node] = seg_t[(node<<1)+1] + seg_t[(node<<1)+2];
             }
         }
-        void _update(int64_t ind, int64_t node, int64_t left, int64_t right){
-            if( left == right - 1){
-                seg_t[node] = seg_t[node] ? 0 : 1;
+        void _update(uint32_t left, uint32_t right, uint32_t node, uint32_t l_seg, uint32_t r_seg){
+           if (right <= l_seg || left >= r_seg) {
+            return;
+        }
+            if(l_seg == r_seg - 1){
+                cout<<'l'<<l_seg<<" r"<<r_seg<<" node "<<node<<'\n';
+                seg_t[node] ^= 1;
                 return;
             }
-            int64_t mid = (left+right)/2;
-            if(ind < mid){
-                _update(ind, 2*node+1, left, mid);
-            } else {
-                _update(ind, 2*node+2, mid, right);
-            }
-            seg_t[node] = seg_t[2*node+1] + seg_t[2*node+2];
+            uint32_t mid = (l_seg+r_seg)/2;
+            _update(left,right, 2*node+1, l_seg, mid);
+            _update(left, right, 2*node+2, mid, r_seg);
+            
+            seg_t[node] = seg_t[node*2 +1] + seg_t[node*2 +2];
         }
-        int64_t  _sum(int64_t left,int64_t  right,int64_t node, int64_t l_seg,int64_t  r_seg){
+        uint32_t  _sum(uint32_t left,uint32_t  right,uint32_t node, uint32_t l_seg,uint32_t  r_seg){
             if(right <= l_seg || left >= r_seg){
                 return 0;
             }
             if (l_seg >= left && r_seg <= right){
                 return seg_t[node];
             }
-            int64_t mid = (l_seg + r_seg)/2;
-            return _sum(left, right, 2*node+1, l_seg, mid) + _sum(left, right, 2*node+2, mid, r_seg);
+            uint32_t mid = (l_seg + r_seg)>>1;
+            return _sum(left, right, (node<<1)+1, l_seg, mid) + _sum(left, right, (node<<1)+2, mid, r_seg);
         } 
 };
 int main(){
-    int64_t n, m, oper, l, r;
+    uint32_t n, m, oper, l, r;
     cin>>n>>m;
-    vector<int64_t>numbers(n, 0);
-    vector<int64_t>rez;
+    vector<uint32_t>numbers(n, 0);
+    vector<uint32_t>rez;
     resize(numbers);
     Tree tree(numbers);
-    for(int64_t i=0; i<m; i++){
+    for(uint32_t i=0; i<m; i++){
         cin>>oper>>l>>r;
         l--;
         r--;
         if(oper == 0){
-            for(int64_t j=l; j<=r; j++){
-                tree.update(j);
-            }
+            tree.update(l, r);
         } else {
             rez.push_back(tree.sum(l, r+1));
         }
+        tree.print_tree();
     }   
     for(auto el: rez){
         cout<<el<<'\n';
