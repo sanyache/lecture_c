32cb7904a12e4c86f69e5728047da03735b1e6cf
sanyache <sanyache75@gmail.com>
2025-02-19 17:31:27 +0200

    change getline

--- string/c_style.cpp
+++ string/c_style.cpp
@@ -10,15 +10,15 @@ int main()
         std::cout << int(mystring[index]) << " ";
     //mystring[6] = 'a';
     //mystring[7] = 'b';
-    std::cout<<mystring;
+    std::cout<<mystring<<'\n';
     int cod_letter = 97;
     char ch = cod_letter;
     cout<<ch<<'\n';
     char letter = 65 + 2;
     cout<<letter<<'\n';
-    cout<<"'1'+'0'="<<'1'+ '0'<<'\t'<<char('1' + '0');
+    cout<<"'1'+'0'="<<'1'+ '0'<<'\t'<<char('1' + '0')<<'\n';
     char ch_dig = '5';
-    cout<<ch_dig - '0' + 6;
+    cout<<ch_dig - '0' + 6<<'\n';
     cout<<char('z'-'a'+'A')<<'\n';
     char l = 'A';
     cout<<islower(l);
--- string/getline.cpp
+++ string/getline.cpp
@@ -4,20 +4,21 @@ using namespace std;
 
 int main(){
 
-    string name, first_name, last_name, part_name;
+    string name, first_name, last_name;
     //cin>>first_name>>last_name;
     //cout<<first_name<<"   "<<last_name;
     getline(cin, name);
     string max_word = "";
     string min_word = name;
     cout<<name<<'\n';
-    stringstream stream_s(name);
-    while(getline(stream_s, part_name, ' ')){
+    stringstream ss(name);
+    string part_name;
+    while(ss >> part_name){
         if(part_name.size() > max_word.size()) max_word = part_name;
         if(part_name.size() < min_word.size()) min_word = part_name;
         //cout<<part_name.size()<<'\n';
     }
-    cout<<"min word"<<min_word<<'\n';
-    cout<<"max word"<<max_word<<'\n';
+    cout<<"min word: "<<min_word<<'\n';
+    cout<<"max word: "<<max_word<<'\n';
     return 0;
 }
